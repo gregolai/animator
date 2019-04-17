7dd83077784dca2d274ce5fd1083d34c06a683f2
Gregory Dalton <gdalton@squarespace.com>
2019-04-17 13:53:18 -0400

    p

--- src/anim/utils/cssProps.js
+++ src/anim/utils/cssProps.js
@@ -1,5 +1,7 @@
 import color from 'color';
 
+const toPx = v => (typeof v === 'number' ? `${v}px` : v);
+
 const parsers = {
   color: v => color(v).rgb().string(),
   number: v => v => parseFloat(v)
@@ -21,7 +23,48 @@ const lerps = {
   }
 }
 
+const createMarginProp = (name) => ({
+  defaultValue: 0,
+  name: 'top',
+  lerp: lerps.number,
+  type: inputTypes.number
+})
+
 const asMap = {
+  margin: {
+    name: 'margin',
+    cssName: 'margin',
+    format: ([top, right, bottom, left]) => {
+      return `${toPx(top)} ${toPx(right)} ${toPx(bottom)} ${toPx(left)}`;
+    },
+    parse: v => {
+      if (typeof v === 'string') {
+        v = v.split(' ');
+      } else {
+        v = [v, v, v, v];
+      }
+
+      // Make every element a number (assume px for now)
+      v = v.map(x => parseFloat(x));
+
+      switch (v.length) {
+        case 1:
+          return [v[0], v[0], v[0], v[0]]; // Apply to all four sides
+        case 2:
+          return [v[0], v[1], v[0], v[1]]; // vertical | horizontal
+        case 3:
+          return [v[0], v[1], v[2], v[1]]; // top | horizontal | bottom
+        default:
+          return [v[0], v[1], v[2], v[3]];
+      }
+    },
+    groups: [
+      createMarginProp('top'),
+      createMarginProp('right'),
+      createMarginProp('bottom'),
+      createMarginProp('left')
+    ]
+  },
   backgroundColor: {
     name: 'backgroundColor',
     cssName: 'background-color',
@@ -49,8 +92,8 @@ const asMap = {
 };
 
 const asArray = Object.keys(asMap)
-    .map(name => asMap[name])
-    .sort((a, b) => a.name < b.name ? -1 : 1);
+  .map(name => asMap[name])
+  .sort((a, b) => a.name < b.name ? -1 : 1);
 
 const reverseLookup = Object.keys(asMap).reduce((map, name) => {
   const entry = asMap[name];
