fb0ef75fc5f90cfef86c192e1a7e960c43a9dee4
Gregory Dalton <gregolai@gmail.com>
2022-01-18 15:55:37 -0600

    p

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -193,21 +193,6 @@ module.exports = function (webpackEnv) {
 							use: getStyleLoaders()
 						},
 
-						// {
-						// 	test: /\.scss$/,
-						// 	include: paths.appSrc,
-						// 	use: getStyleLoaders(
-						// 		{
-						// 			importLoaders: 1,
-						// 			sourceMap: isEnvProduction,
-						// 			modules: {
-						// 				localIdentName: '[name]__[local]___[hash:base64:5]'
-						// 			}
-						// 		},
-						// 		'sass-loader'
-						// 	)
-						// },
-
 						// "file" loader makes sure those assets get served by WebpackDevServer.
 						// When you `import` an asset, you get its (virtual) filename.
 						// In production, they would get copied to the `build` folder.
